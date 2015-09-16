5f654ab6287408c8bdfe931d0e323288d11dff0b
Tom Martin <tom@tpgmartin.com>
2015-09-16 22:54:33 +0100

    Check end of game condition

--- js/t3.js
+++ js/t3.js
@@ -41,44 +41,53 @@ function cyclePlayerToken(turn) {
     return turn % 2 === 0 ? 'X' : 'O';
 }
 
-function updateGrid(ref, grid, token) {
+function updateGrid(ref, grid, token, turn) {
+
     var refArray = ref.toString().split(',');
 
     grid[refArray[0]][refArray[1]] = token;
 
     $('[data-ref="'+ref+'"]').text(token);
 
-    checkGrid(grid);
+    checkGrid(grid, turn);
+
 }
 
-function checkGrid(grid) {
+function checkGrid(grid, turn) {
     if (grid[0][0] + grid[0][1] + grid[0][2] === 'XXX' || grid[1][0] + grid[1][1] + grid[1][2] === 'XXX' || grid[2][0] + grid[2][1] + grid[2][2] === 'XXX') {
         alert('Winner Player X');
         var grid = clearGrid(grid);
-        newGame(grid);
+        newGame()
     } else if (grid[0][0] + grid[1][0] + grid[2][0] === 'XXX' || grid[0][1] + grid[1][1] + grid[2][1] === 'XXX' || grid[0][2] + grid[1][2] + grid[2][2] === 'XXX') {
         alert('Winner Player X');
         var grid = clearGrid(grid);
-        newGame(grid);
+        newGame()
     } else if (grid[0][0] + grid[1][1] + grid[2][2] === 'XXX' || grid[2][0] + grid[1][1] + grid[0][2] === 'XXX') {
         alert('Winner Player X');
         var grid = clearGrid(grid);
-        newGame(grid);
+        newGame()
     } else if (grid[0][0] + grid[0][1] + grid[0][2] === 'OOO' || grid[1][0] + grid[1][1] + grid[1][2] === 'OOO' || grid[2][0] + grid[2][1] + grid[2][2] === 'OOO') {
         alert('Winner Player O');
         var grid = clearGrid(grid);
-        newGame(grid);
+        newGame()
     } else if (grid[0][0] + grid[1][0] + grid[2][0] === 'OOO' || grid[0][1] + grid[1][1] + grid[2][1] === 'OOO' || grid[0][2] + grid[1][2] + grid[2][2] === 'OOO') {
         alert('Winner Player O');
         var grid = clearGrid(grid);
-        newGame(grid);
+        newGame()
     } else if (grid[0][0] + grid[1][1] + grid[2][2] === 'OOO' || grid[2][0] + grid[1][1] + grid[0][2] === 'OOO') {
         alert('Winner Player O');
         var grid = clearGrid(grid);
-        newGame(grid);
-    } else {
-        return;
+        newGame();
+    } else if (turn > 7) {
+        alert('Game ends in a draw');
+        var grid = clearGrid(grid);
+        newGame();
     }
+}
+
+function playGame(turn) {
+
+
 }
 
 function newGame() {
